5eb02511fe707c62e5bc0e4d107aaf0010326547
jasleenwahi <jasleen.wahi@knoldus.com>
2023-04-13 18:49:43 +0530

    Maven Assignment

---
+++ service/src/main/java/com/example/myproject/service/Main.java
@@ -0,0 +1,8 @@
+package com.example.myproject.service;
+
+public class Main {
+    public static void main(String[] args) {
+        Service service = new Service();
+        System.out.println(service.returnString());
+    }
+}
---
+++ service/src/main/java/com/example/myproject/service/Service.java
@@ -0,0 +1,8 @@
+package com.example.myproject.service;
+
+public class Service {
+
+    public String returnString(){
+        return "Hello!";
+    }
+}
