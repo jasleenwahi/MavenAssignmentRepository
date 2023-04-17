101093dc88503c7e028abd0fa336202c3c1dea36
jasleenwahi <jasleen.wahi@knoldus.com>
2023-04-17 14:22:13 +0530

    Maven assignment

--- webapp/src/main/java/com/example/myproject/webapp/WebApp.java
+++ webapp/src/main/java/com/example/myproject/webapp/WebApp.java
@@ -7,13 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class WebApp {
-
-    @GetMapping("/hello")
-    public String returnHelloWorld(){
-        return "Hello World!";
-    }
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
     }
